631bd4d95bb5b78c76e967eb7f330056860cd745
Dharanidhar43 <96573512+Dharanidhar43@users.noreply.github.com>
2023-10-25 15:46:46 +0530

    Add files via upload

---
+++ ReactTask6-main/src/components/Contact.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>I'm always open to new opportunities and collaborations.</p>
+      <ul>
+        <li>
+          <strong>Email:</strong>{" "}
+          <a href="mailto:dharanidharpushkarreddy@gmail.com">
+            dharanidharpushkarreddy@gmail.com
+          </a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong>{" "}
+          <a
+            href="https://www.linkedin.com/in/dharanidhar-pushkar-reddy-6b873822/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            click here
+          </a>
+        </li>
+        <li>
+          <strong>GitHub:</strong>{" "}
+          <a
+            href="https://github.com/Dharanidhar43"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            click here
+          </a>
+        </li>
+      </ul>
+      <p>I eagerly anticipate the opportunity to connect with you!</p>
+    </section>
+  );
+};
+
+export default Contact;
